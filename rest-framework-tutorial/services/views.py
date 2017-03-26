24957689d3164b0af90dd2536b2bd2219aa764b9
josip-milic <josip.milic@gmail.com>
2017-03-26 20:50:39 +0200

    Service update

--- rest-framework-tutorial/services/views.py
+++ rest-framework-tutorial/services/views.py
@@ -3,6 +3,59 @@ from .models import Event
 from rest_framework import viewsets
 from .serializer import EventSerializer
 
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
 class EventViewSet(viewsets.ReadOnlyModelViewSet):
-	queryset = Event.objects.all()
-	serializer_class = EventSerializer
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+	
+class EventList(APIView):
+    """
+    List all event, or create a new event.
+    """
+    def get(self, request, format=None):
+        events = Event.objects.all()
+        serializer = EventSerializer(events, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = EventSerializer(data=request.DATA)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        event = self.get_object(pk)
+        event.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)	
+
+class EventDetail(APIView):
+    """
+    Retrieve, update or delete a event instance.
+    """
+    def get_object(self, pk):
+        try:
+            return Event.objects.get(pk=pk)
+        except Event.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        event = self.get_object(pk)
+        event = EventSerializer(event)
+        return Response(event.data)
+
+    def put(self, request, pk, format=None):
+        event = self.get_object(pk)
+        serializer = EventSerializer(event, data=request.DATA)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        event = self.get_object(pk)
+        event.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
