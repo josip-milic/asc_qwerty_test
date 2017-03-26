1a8f8e85174d95ab50984e005f8e59182bc736cd
josip-milic <josip.milic@gmail.com>
2017-03-26 21:03:19 +0200

    Service update

--- rest-framework-tutorial/services/urls.py
+++ rest-framework-tutorial/services/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from services import views
 
 urlpatterns = [
-    url(r'^events/$', views.EventList),
-    url(r'^events/(?P<pk>[0-9]+)/$', views.EventDetail),
+    url(r'^events/$', views.EventList.get),
+    url(r'^events/(?P<pk>[0-9]+)/$', views.EventDetail.get),
 ]
