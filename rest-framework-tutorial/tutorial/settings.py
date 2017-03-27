6b953a00d1ce5ad6fa072a06783bf94157e82faa
josip-milic <josip.milic@gmail.com>
2017-03-27 19:13:38 +0200

    Service connect

--- rest-framework-tutorial/tutorial/settings.py
+++ rest-framework-tutorial/tutorial/settings.py
@@ -84,11 +84,6 @@ WSGI_APPLICATION = 'tutorial.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-	,
-    'mysql': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'asc_qwerty',
         'USER': 'root',
