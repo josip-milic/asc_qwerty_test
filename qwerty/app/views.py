8fc51cc502d120d61f8e441b041e5f592d53581d
josip-milic <josip.milic@gmail.com>
2017-03-30 20:30:46 +0200

    app upload

--- qwerty/app/views.py
+++ qwerty/app/views.py
@@ -3,6 +3,7 @@ from models import Event
 
 def index(request):
     all_entries = Event.objects.all()
-    print('aaa')
-    print(all_entries)
+    g = open('test.txt','w+')
+    g.write('aaa')
+    g.write(all_entries[0].id)
     return HttpResponse("Hello, world. You're at the polls index.")
