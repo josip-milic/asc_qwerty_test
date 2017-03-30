f399cb06a32524b9dec30009767aca49469f3df4
josip-milic <josip.milic@gmail.com>
2017-03-30 20:33:55 +0200

    app upload

--- qwerty/app/views.py
+++ qwerty/app/views.py
@@ -3,6 +3,8 @@ from models import Event
 
 def index(request):
     all_entries = Event.objects.all()
+    print('aa')
+    print(all_entries)
     g = open('test.txt','w+')
     g.write('aaa')
     g.write(all_entries[0].id)
