9d3eca0c0443ba94de1cd7e8d74d3b2686e10879
josip-milic <josip.milic@gmail.com>
2017-04-01 13:23:24 +0200

    return index.html

--- qwerty/app/views.py
+++ qwerty/app/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
 from models import Event
+from django.shortcuts import render
 
 def index(request):
     all_entries = Event.objects.all()
     print(all_entries)
+    return render(request, 'index.html', {'test': 'aaa'})
     return HttpResponse("Hello, world.")
